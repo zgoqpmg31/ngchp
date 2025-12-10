f35c51e139bdd411ae2207eab9f011b3c19d4c27
zgoqpmg31 <rt-official@outlook.jp>
2025-12-10 14:55:42 +0900

    Add files via upload

---
+++ ir/js/script.js
@@ -0,0 +1,566 @@
+particlesJS("particles-js5",
+{
+    "particles": {
+      "number": {
+        "value": 1,
+        "density": {
+          "enable": false,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 10,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 40
+        },
+        "image": {
+          "src": "https://overse.co.jp/renew22/wp-content/themes/overse_v_1-0-1/img/obj05.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 564,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 564,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 100,
+        "color": "#ffffff",
+        "opacity": 0,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 10,
+          "rotateY": 15
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 803.9107415467573,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 500,
+          "size": 40,
+          "duration": 10,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 120,
+          "duration": 1.5
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
+
+
+
+
+
+
+particlesJS("particles-js4",
+{
+    "particles": {
+      "number": {
+        "value": 1,
+        "density": {
+          "enable": false,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 10,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 40
+        },
+        "image": {
+          "src": "https://overse.co.jp/renew22/wp-content/themes/overse_v_1-0-1/img/obj04.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 428,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 428,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 100,
+        "color": "#ffffff",
+        "opacity": 0,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 10,
+          "rotateY": 15
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 803.9107415467573,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 500,
+          "size": 40,
+          "duration": 10,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 120,
+          "duration": 1.5
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
+particlesJS("particles-js3",
+{
+    "particles": {
+      "number": {
+        "value": 1,
+        "density": {
+          "enable": false,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 10,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 40
+        },
+        "image": {
+          "src": "https://overse.co.jp/renew22/wp-content/themes/overse_v_1-0-1/img/obj03.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 502,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 502,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 100,
+        "color": "#ffffff",
+        "opacity": 0,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 10,
+          "rotateY": 15
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 803.9107415467573,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 500,
+          "size": 40,
+          "duration": 10,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 120,
+          "duration": 1.5
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
+particlesJS("particles-js2",
+{
+    "particles": {
+      "number": {
+        "value": 1,
+        "density": {
+          "enable": false,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 10,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 40
+        },
+        "image": {
+          "src": "https://overse.co.jp/renew22/wp-content/themes/overse_v_1-0-1/img/obj02.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 150,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 150,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 100,
+        "color": "#ffffff",
+        "opacity": 0,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 10,
+          "rotateY": 15
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 803.9107415467573,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 500,
+          "size": 40,
+          "duration": 10,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 120,
+          "duration": 1.5
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
+particlesJS("particles-js",
+{
+    "particles": {
+      "number": {
+        "value": 1,
+        "density": {
+          "enable": false,
+          "value_area": 1000
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "image",
+        "stroke": {
+          "width": 10,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 40
+        },
+        "image": {
+          "src": "https://overse.co.jp/renew22/wp-content/themes/overse_v_1-0-1/img/obj01.png",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 1,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 147,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 147,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 100,
+        "color": "#ffffff",
+        "opacity": 0,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 10,
+          "rotateY": 15
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 803.9107415467573,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 100,
+          "size": 40,
+          "duration": 10,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 120,
+          "duration": 1.5
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
